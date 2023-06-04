1bb24a4361e4d78914be0ee6f8191e590bfa29fa
Scott H. Hawley <scott.hawley@belmont.edu>
2023-06-04 12:24:06 -0500

    first commit of mostly blank project

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vicregaddon',
+    version='0.0.1',
+    author='Scott H. Hawley',
+    author_email='scott.hawley@belmont.edu', 
+    description='A lightweight and modular parallel PyTorch implementation of VICReg (intended for audio, but will try to be general)',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'torchaudio',
+        'numpy',
+        'aeiou',
+        'pytorch-lightning'
+    ],
+)
